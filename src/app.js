a6a2aabaeb79fab5ee010e23132789b68bd7b1ca
krisjhamilton <krisjhamilton@users.noreply.github.com>
2015-11-01 19:44:37 +1300

    Initial Release

--- src/app.js
+++ src/app.js
@@ -6,9 +6,45 @@ var Vector2 = require('vector2');
 var xurResponse = {};
 var xurHere = false;
 
+var splashTextColor = 'yellow';
+var splashBgColor = 'black'; 
+var menuTextColor = 'white';
+var menuTextHLColor = 'black';
+var menuBgColor = 'black';
+var menuBgHLColor = 'yellow';
+
+var basalt = false;
+
+if(Pebble.getActiveWatchInfo) {
+	basalt = true;
+}else{
+	basalt = false;
+}
+
+if(basalt){
+	var watchinfo = Pebble.getActiveWatchInfo();
+	var platform = watchinfo.platform;
+	console.log(platform);
+	console.log('basalt');
+	splashTextColor = 'yellow';
+	splashBgColor = 'black'; 
+	menuTextColor = 'white';
+	menuBgColor = 'black';
+	menuTextHLColor = 'black';
+	menuBgHLColor = 'yellow';
+}else{
+	console.log('aplite');
+	splashTextColor = 'white';
+	splashBgColor = 'black'; 
+	menuTextColor = 'black';
+	menuBgColor = 'white';
+	menuTextHLColor = 'white';
+	menuBgHLColor = 'black';
+}
+
 // Show splash screen while waiting for data
 var splashWindow = new UI.Window({
-	backgroundColor: 'blueMoon',
+	backgroundColor: 'black',
 	fullscreen: true
 });
 
@@ -18,10 +54,10 @@ var text = new UI.Text({
 	size: new Vector2(144, 168),
 	text:'Checking if Xur \n is around',
 	font:'GOTHIC_14_BOLD',
-	color:'white',
+	color: splashTextColor,
 	textOverflow:'wrap',
 	textAlign:'center',
-	backgroundColor:'blueMoon'
+	backgroundColor: splashBgColor
 });	
 
 // Second Text element to inform user
@@ -30,17 +66,17 @@ var text2 = new UI.Text({
 	size: new Vector2(144, 168),
 	text:'Xur in not \n around at the \n moment',
 	font:'GOTHIC_14_BOLD',
-	color:'white',
+	color: splashTextColor,
 	textOverflow:'wrap',
 	textAlign:'center',
-	backgroundColor:'blueMoon'
+	backgroundColor: splashBgColor
 });	
 
 var image = new UI.Image({
 	position: new Vector2(58, 110),
 	size: new Vector2(26, 24),
-	compositing: 'or',
-	image: 'images/main_logo_t_w.png'
+	image: 'images/main_logo_t_w.png',
+	compositing: 'or'
 });
 
 splashWindow.add(text);
@@ -55,141 +91,36 @@ function parseFeed(data) {
 		if (placeHolder.hasOwnProperty(key)) {
 			var val = placeHolder[key];
 			console.log(val);
-			var itemName;
-			var itemTypeName;
-			var itemDescription;
-			var classType;
-			switch (val.itemTypeName) {
-				case "Gauntlets":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
+
+			function itemInfo(){
+				var itemName;
+				var itemTypeName;
+				var itemDescription;
+				var classType;
+				var itemType;
+				itemName = val.itemName;
+				itemTypeName = val.itemTypeName;
+				itemDescription = val.itemDescription;
+				classType = val.classType;
+				itemType = val.itemType;
+				items.push({
+					title: itemTypeName,
+					subtitle: itemName,
+					body: itemDescription,
+					classType: classType
+				});
+			}
+			switch (val.itemType) {
+				case 2:
+					itemInfo();
 					break;
-				case "Helmet":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
+				case 3:
+					itemInfo();
 					break;
-				case "Chest Armor":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Leg Armor":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Helmet Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Body Armor Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Leg Armor Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Gauntlet Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Primary Weapon Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Special Weapon Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
-					break;
-				case "Heavy Weapon Engram":
-					itemName = val.itemName;
-					itemTypeName = val.itemTypeName;
-					itemDescription = val.itemDescription;
-					classType = val.classType;
-					items.push({
-						title: itemTypeName,
-						subtitle: itemName,
-						body: itemDescription,
-						classType: classType
-					});
+				case 8:
+					itemInfo();
 					break;
 				default:
-
 					break;
 			}
 		}
@@ -223,9 +154,11 @@ ajax(
 				var menuItems = parseFeed(xurResponse);
 				//console.log(menuItems);
 				var resultsMenu = new UI.Menu({
-					highlightTextColor: 'white',
-					highlightBackgroundColor: 'blueMoon',
-					
+					backgroundColor: menuBgColor,
+					textColor: menuTextColor,
+					highlightTextColor: menuTextHLColor,
+					highlightBackgroundColor: menuBgHLColor,
+
 					//backgroundColor: 'VividCerulean',
 					sections: [{
 						title: "Xur's Items",
@@ -237,62 +170,62 @@ ajax(
 				resultsMenu.on('select', function(e) {	
 					if(e.item.classType === 0){
 						var card1 = new UI.Card({
-							bodyColor: 'white',
-							titleColor:"white",
-							subtitleColor:"white",
+							bodyColor: 'black',
+							titleColor:"black",
+							subtitleColor:"black",
 							title: 'Titan ' + e.item.title,
 							subtitle: e.item.subtitle,
 							body: 'Description: \n' +e.item.body,
 							scrollable: true,
 							style: "small",
-							backgroundColor: 'blueMoon',
+							backgroundColor: 'yellow',
 							//textColor: 'white'
 						});
 						card1.show();
 					}else if(e.item.classType === 1){
 						var card2 = new UI.Card({
-							bodyColor: 'white',
-							titleColor:"white",
-							subtitleColor:"white",
+							bodyColor: 'black',
+							titleColor:"black",
+							subtitleColor:"black",
 							title: 'Hunter ' + e.item.title,
 							subtitle: e.item.subtitle,
 							body: 'Description: \n' +e.item.body,
 							scrollable: true,
 							style: "small",
-							backgroundColor: 'blueMoon',
+							backgroundColor: 'yellow',
 							//textColor: 'white'
 						});
 						card2.show();
 					}else if(e.item.classType === 2){
 						var card3 = new UI.Card({
-							bodyColor: 'white',
-							titleColor:"white",
-							subtitleColor:"white",
+							bodyColor: 'black',
+							titleColor:"black",
+							subtitleColor:"black",
 							title: 'Warlock ' + e.item.title,
 							subtitle: e.item.subtitle,
 							body: 'Description: \n' +e.item.body,
 							scrollable: true,
 							style: "small",
-							backgroundColor: 'blueMoon',
+							backgroundColor: 'yellow',
 							//textColor: 'white'
 						});
 						card3.show();
 					}else{
 						var card4 = new UI.Card({
-							bodyColor: 'white',
-							titleColor:"white",
-							subtitleColor:"white",
+							bodyColor: 'black',
+							titleColor:"black",
+							subtitleColor:"black",
 							title: e.item.title,
 							subtitle: e.item.subtitle,
 							body: '\n Description: \n' +e.item.body,
 							scrollable: true,
 							style: "small",
-							backgroundColor: 'blueMoon',
+							backgroundColor: 'yellow',
 							//textColor: 'white'
 						});
 						card4.show();
 					}
-					
+
 				});
 				resultsMenu.show();
 				splashWindow.hide();
@@ -307,108 +240,108 @@ ajax(
 
 // WIP
 /*
-														// Add an action for SELECT
-														resultsMenu.on('select', function(e) {
-															//var menuItems2 = parseFeed2(data);
-															// Construct Menu to show to user
-															var powerLevel = data.Response.data.characters[e.itemIndex].characterBase.powerLevel;
-															var defense = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_DEFENSE.value;
-															var decipline = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_DISCIPLINE.value;
-															var intellect = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_INTELLECT.value;
-															var strength = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_STRENGTH.value;
-															var armor = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_ARMOR.value;
-															var agility = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_AGILITY.value;
-															var recovery = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_RECOVERY.value;
-															var glimmer = data.Response.data.inventory.currencies[0].value;
-															var grimoireScore = data.Response.data.grimoireScore;
-															var charId = data.Response.data.characters[e.itemIndex].characterBase.characterId;
+																// Add an action for SELECT
+																resultsMenu.on('select', function(e) {
+																	//var menuItems2 = parseFeed2(data);
+																	// Construct Menu to show to user
+																	var powerLevel = data.Response.data.characters[e.itemIndex].characterBase.powerLevel;
+																	var defense = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_DEFENSE.value;
+																	var decipline = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_DISCIPLINE.value;
+																	var intellect = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_INTELLECT.value;
+																	var strength = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_STRENGTH.value;
+																	var armor = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_ARMOR.value;
+																	var agility = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_AGILITY.value;
+																	var recovery = data.Response.data.characters[e.itemIndex].characterBase.stats.STAT_RECOVERY.value;
+																	var glimmer = data.Response.data.inventory.currencies[0].value;
+																	var grimoireScore = data.Response.data.grimoireScore;
+																	var charId = data.Response.data.characters[e.itemIndex].characterBase.characterId;
 
-															var charUrl = "http://www.bungie.net/Platform/Destiny/"+ platform +"/Account/"+ memId +"/Character/"+ charId +"/progression/";
-															console.log(charUrl);
-															produceNow();
-															function produceNow(){
-																ajax(
-																	{ 
-																		url: charUrl, 
-																		type: 'json'
-																	}, function(data3) {
-																		var vanLv = data3.Response.data.progressions[15].level;
-																		var vanProg = data3.Response.data.progressions[15].progressToNextLevel;
-																		var vanNext = data3.Response.data.progressions[15].nextLevelAt;
-																		var cruLv = data3.Response.data.progressions[16].level;
-																		var cruProg = data3.Response.data.progressions[16].progressToNextLevel;
-																		var cruNext = data3.Response.data.progressions[16].nextLevelAt;
-																		var cripLv = data3.Response.data.progressions[11].level;
-																		var cripProg = data3.Response.data.progressions[11].progressToNextLevel;
-																		var cripNext = data3.Response.data.progressions[11].nextLevelAt;
+																	var charUrl = "http://www.bungie.net/Platform/Destiny/"+ platform +"/Account/"+ memId +"/Character/"+ charId +"/progression/";
+																	console.log(charUrl);
+																	produceNow();
+																	function produceNow(){
+																		ajax(
+																			{ 
+																				url: charUrl, 
+																				type: 'json'
+																			}, function(data3) {
+																				var vanLv = data3.Response.data.progressions[15].level;
+																				var vanProg = data3.Response.data.progressions[15].progressToNextLevel;
+																				var vanNext = data3.Response.data.progressions[15].nextLevelAt;
+																				var cruLv = data3.Response.data.progressions[16].level;
+																				var cruProg = data3.Response.data.progressions[16].progressToNextLevel;
+																				var cruNext = data3.Response.data.progressions[16].nextLevelAt;
+																				var cripLv = data3.Response.data.progressions[11].level;
+																				var cripProg = data3.Response.data.progressions[11].progressToNextLevel;
+																				var cripNext = data3.Response.data.progressions[11].nextLevelAt;
 
 
-																		//console.log(vanExp);
-																		var charDetails = new UI.Menu({
-																			sections: [{
-																				title: e.item.title,
-																				items: [{
-																					title: "Power Level",
-																					subtitle: powerLevel
-																				},{
-																					title: "Glimmer",
-																					subtitle: glimmer + " / 25000"
-																				},{
-																					title: "Grimoire Score",
-																					subtitle: grimoireScore
-																				}]
-																			},{
-																				title: "Progression",
-																				items: [{
-																					title: "Vanguard Level",
-																					subtitle: vanLv
-																				},{
-																					title: "Vanguard Exp",
-																					subtitle: vanProg + " / " + vanNext
-																				},{
-																					title: "Cruicible Level",
-																					subtitle: cruLv
-																				},{
-																					title: "Cruicible Exp",
-																					subtitle: cruProg + " / " + cruNext
-																				},{
-																					title: "Cryptarch Level",
-																					subtitle: cripLv
-																				},{
-																					title: "Cryptarch Exp",
-																					subtitle: cripProg + " / " + cripNext
-																				}]
-																			},{
-																				title: "Primary Stats",
-																				items: [{
-																					title: "Defense",
-																					subtitle: defense
-																				},{
-																					title: "Intellect",
-																					subtitle: intellect
-																				},{
-																					title: "Decipline",
-																					subtitle: decipline
-																				},{
-																					title: "Strength",
-																					subtitle: strength
-																				}]
-																			},{
-																				title: "Secondary Stats",
-																				items: [{
-																					title: "Armor",
-																					subtitle: armor + " / 10"
-																				},{
-																					title: "Agility",
-																					subtitle: agility + " / 10"
-																				},{
-																					title: "Recovery",
-																					subtitle: recovery + " / 10"
-																				}]
-																			}]
-																		});
-																		charDetails.show();
-																	});
-															}
-														});
-														*/
+																				//console.log(vanExp);
+																				var charDetails = new UI.Menu({
+																					sections: [{
+																						title: e.item.title,
+																						items: [{
+																							title: "Power Level",
+																							subtitle: powerLevel
+																						},{
+																							title: "Glimmer",
+																							subtitle: glimmer + " / 25000"
+																						},{
+																							title: "Grimoire Score",
+																							subtitle: grimoireScore
+																						}]
+																					},{
+																						title: "Progression",
+																						items: [{
+																							title: "Vanguard Level",
+																							subtitle: vanLv
+																						},{
+																							title: "Vanguard Exp",
+																							subtitle: vanProg + " / " + vanNext
+																						},{
+																							title: "Cruicible Level",
+																							subtitle: cruLv
+																						},{
+																							title: "Cruicible Exp",
+																							subtitle: cruProg + " / " + cruNext
+																						},{
+																							title: "Cryptarch Level",
+																							subtitle: cripLv
+																						},{
+																							title: "Cryptarch Exp",
+																							subtitle: cripProg + " / " + cripNext
+																						}]
+																					},{
+																						title: "Primary Stats",
+																						items: [{
+																							title: "Defense",
+																							subtitle: defense
+																						},{
+																							title: "Intellect",
+																							subtitle: intellect
+																						},{
+																							title: "Decipline",
+																							subtitle: decipline
+																						},{
+																							title: "Strength",
+																							subtitle: strength
+																						}]
+																					},{
+																						title: "Secondary Stats",
+																						items: [{
+																							title: "Armor",
+																							subtitle: armor + " / 10"
+																						},{
+																							title: "Agility",
+																							subtitle: agility + " / 10"
+																						},{
+																							title: "Recovery",
+																							subtitle: recovery + " / 10"
+																						}]
+																					}]
+																				});
+																				charDetails.show();
+																			});
+																	}
+																});
+																*/
